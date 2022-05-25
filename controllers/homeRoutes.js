4216d17cf377e1c86e5821c2d6cafd4adc577cfe
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-25 06:37:13 -0600

    Deleted landing page from HW example, need to recreate

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,23 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+//     Need to show login/register options on landing page
     });
   } catch (err) {
     res.status(500).json(err);
