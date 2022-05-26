724b61bbcadceefcb9a390a0c24a3e847cfff0ab
Natalya Colburn <greatqueenbee@gmail.com>
2022-05-25 19:14:19 -0600

    taking changes back

--- models/User.js
+++ models/User.js
@@ -39,24 +39,15 @@ User.init(
   {
     hooks: {
       beforeCreate: async (newUserData) => {
-        try {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
-      } catch (err) {
-        console.log(err);
-        return err;
-      }
       },
       beforeUpdate: async (updatedUserData) => {
-        try {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
-      } catch (err) {
-        console.log(err);
-        return err;
-      }
+      }, 
       },
-    },
+    
     sequelize,
     timestamps: false,
     freezeTableName: true,
