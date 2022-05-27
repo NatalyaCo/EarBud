ca1bee67b8ec314505569b3ec6794f69b41d8532
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-26 19:37:52 -0600

    created some routes and added comments for what each display should eventually include

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,19 +4,47 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+    // Use Spotify API to have a "log in with my spotify credentials" button that logs in if user is already in database 
+    // or creates user in database using Spotify credentials with some more fields to fill out to complete profile
+    res.render("login");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
+router.get('/register', async (req, res) => {
+  try {
+    // Display register page with username pre-populated to match Spotify user name, description about yourself, photo, & email
+  } catch (err) {
+    res.status(500).json(err);
   }
-
-  res.render('login');
 });
+
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Display users profile information, some info from their Spotify profile (top songs, etc) and list of friends they've connected to
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/matchEngine', async (req, res) => {
+  try {
+    // Displays match section where app suggests friends that have similar music interest, displays their profile info and gives
+    // user option to try to match with suggestion or ignore. (need functionality to not suggest user who are already friends or rejects)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/user/:id', async (req, res) => {
+  try {
+    // Displays profile for user, but only if you are already matched/friends with
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
