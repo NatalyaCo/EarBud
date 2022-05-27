3b1a4313fd316581a236c0e155b2693eb5cdf5bd
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-26 20:54:58 -0600

    added preference post route

--- controllers/api/preferenceRoutes.js
+++ controllers/api/preferenceRoutes.js
@@ -1,16 +1,13 @@
 const router = require('express').Router();
 const { Preference } = require('../../models');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
-    const preferenceData = await Preference.create(req.body);
-
-    req.session.save(() => {
-      req.session.preference_id = preferenceData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(preferenceData);
+    const preferenceData = await Preference.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
+    res.status(200).json(preferenceData);
   } catch (err) {
     res.status(400).json(err);
   }
