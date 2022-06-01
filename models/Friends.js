3efd400382df2f423fa788a5a80a9a367622aa5c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-05-31 18:59:40 -0600

    created Friends model

---
+++ models/Friends.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Friends extends Model {}
+
+Friends.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    matched_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'preference',
+  }
+);
+
+module.exports = Preference;
