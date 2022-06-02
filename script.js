7d29bd1cf19f3f0325792bcd5512c2e562828fe1
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-01 20:55:41 -0600

    commented out the drop down

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var options = {
-      dropdownOptions: {
-          alignment: 'right',
-          hover: true
-      }
-  }
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems, options);
-  });
+// document.addEventListener('DOMContentLoaded', function () {
+//   var options = {
+//       dropdownOptions: {
+//           alignment: 'right',
+//           hover: true
+//       }
+//   }
+//   var elems = document.querySelectorAll('.dropdown-trigger');
+//   var instances = M.Dropdown.init(elems, options);
+//   });
 
   document.addEventListener('DOMContentLoaded', function() {
 
