c193b339bada49ee2f425de2312717d67d9a3a3b
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-01 20:49:31 -0600

    did drp down form in mockup html

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, {});
+  });
+  
