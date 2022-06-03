f995ee7656d8834f1e8fc324232e59e71e0f6737
Natalya Colburn <greatqueenbee@gmail.com>
2022-06-03 12:18:42 -0600

    tutor work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,6 +41,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
     });
     // Serialize user data
     const user = userData.get({ plain: true });
+    console.log(user)
     // Pass serialized data and session into dashboard template
     res.render('dashboard', {
       ...user,
@@ -94,6 +95,23 @@ router.get('/user/:id', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+  // try {
+    const userData = await User.findByPk(req.session.user_id, {
+           
+    })
+    const user= userData.get({ plain: true});
+    res.render('profile', {
+      ...user,
+      logged_in: req.session.logged_in,
+  
+    });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+  
 });
 
 module.exports = router;
