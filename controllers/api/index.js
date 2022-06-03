6232167e21f7ebe8abdab506254f9bc5d4920376
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-06-03 11:42:25 -0600

    added preferences and friends routes to index file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const preferenceRoutes = require('./preferenceRoutes');
+const friendsRoutes = require('./friendsRoutes');
 
 router.use('/users', userRoutes);
+router.use('/preferences', preferenceRoutes);
+router.use('/friends', friendsRoutes);
 
 module.exports = router;
