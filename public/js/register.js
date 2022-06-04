ffe2a318a701a5c89db6320e3730f290b2d3e66c
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-06-04 13:08:29 -0600

    first stab at event listener to save preferences with user_id included

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,35 @@
 const submitFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const about_me = document.querySelector('#about_me').value.trim();
+  const fave_genre = document.querySelector('#fave_genre').value.trim();
+  const fave_experience_style = document
+    .querySelector('#fave_experience_style')
+    .value.trim();
+  const fave_decade = document.querySelector('#fave_decade').value.trim();
+  const intentions = document.querySelector('#intentions').value.trim();
+  const profile_pic = result.info.url;
+  const user_id = req.session.user_id;
 
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
+  if (
+    about_me &&
+    fave_genre &&
+    fave_experience_style &&
+    fave_decade &&
+    intentions &&
+    profile_pic
+  ) {
+    const response = await fetch('/api/preferences', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({
+        about_me,
+        fave_genre,
+        fave_experience_style,
+        fave_decade,
+        intentions,
+        profile_pic,
+        user_id,
+      }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -21,5 +42,5 @@ const submitFormHandler = async (event) => {
 };
 
 document
-  .querySelector('.submit-pref')
+  .querySelector('.save_pref')
   .addEventListener('submit', submitFormHandler);
